6c5ac60801e95fb8e4675ce44cc277bed34c8a66
Martin Beneš <martinbenes1996@gmail.com>
2020-04-23 10:41:35 +0200

    Slovenia

---
+++ R/ministrstvoZaZdravje.R
@@ -0,0 +1,32 @@
+ministrstvoZaZdravje <- function(cache, level){
+  # author: Martin Benes
+  
+  # source: Ministery of Health, Slovenia
+  url <- 'https://www.gov.si/assets/vlada/Koronavirus-podatki/en/EN_Covid-19-all-data.xlsx'
+  
+  # download
+  x <- read_excel(url, cache=cache)
+  
+  # format
+  if(level==1){
+    
+    colnames(x) <- mapvalues(colnames(x), c(
+      'Date'                                         = 'date',
+      'Tested (all)'                                 = 'tests',
+      'Positive (all)'                               = 'confirmed',
+      'All hospitalized on certain day'              = 'hospitalized',
+      'All persons in intensive care on certain day' = 'icu',
+      'Discharged'                                   = 'recovered',
+      'Deaths (all)'                                 = 'deaths'
+    ))
+    
+    #x$date         <- as.Date(x$date, format="%Y.%m.%d.")
+    x$recovered <- cumsum(x$recovered)
+    
+  }
+  
+  x$country <- 'SVN'
+  
+  return(x)
+  
+}
