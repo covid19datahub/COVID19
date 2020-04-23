df5cabf47cff334a187d0d62a8466825a33f6367
Martin Beneš <martinbenes1996@gmail.com>
2020-04-23 10:41:35 +0200

    Slovenia

---
+++ R/SVN.R
@@ -0,0 +1,20 @@
+SVN <- function(level, cache){
+  # Author: Martin Benes
+    
+  # fallback
+  if(level>1)
+    return(NULL)
+    
+  # download
+  x <- ministrstvoZaZdravje(cache = cache, level = level)
+    
+  # id
+  if(level==1)
+    x$id <- "SVN"
+    
+  # return
+  return(x)
+    
+}
+
+SVN(1, T)
