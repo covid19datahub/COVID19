6d2145284dde43142f7eee99a79464dd3a8c7667
emanuele-guidotti <emanuele.guidotti@hotmail.it>
2020-06-16 12:38:00 +0200

    Clean file structure

--- R/covid19turkey.R
+++
@@ -1,8 +0,0 @@
-covid19turkey <- function(level, cache){
-  read.csv("https://raw.githubusercontent.com/ozanerturk/covid19-turkey-api/master/dataset/timeline.csv")
-  
-  covid19turkey <- covid19turkey[ , -which(names(covid19turkey)%in%c("totalIntensiveCare", "totalIntubated"))]
-  
-  covid19turkey <- covid19turkey[ ,c(1 ,6 ,7 ,8 ,9 ,2 ,3 ,4 ,5 )]
-return(covid19turkey)
-}
