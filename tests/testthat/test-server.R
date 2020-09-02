9f985a0716345ac5f875d3a73f3a43e9dc7af578
emanuele-guidotti <emanuele.guidotti@hotmail.it>
2020-09-02 19:08:01 +0200

    Update tests

--- tests/testthat/test-server.R
+++ tests/testthat/test-server.R
@@ -1,6 +1,6 @@
 test_that("server-1", {
-
-  url <- "https://storage.covid19datahub.io/rawdata-1.csv"
+  
+  url <- "https://storage.covid19datahub.io/data-1.csv"
   x   <- read.csv(url)
   
   expect_equal(max(x$date) == Sys.Date(), TRUE)
@@ -9,7 +9,7 @@ test_that("server-1", {
 
 test_that("server-2", {
   
-  url <- "https://storage.covid19datahub.io/rawdata-2.csv"
+  url <- "https://storage.covid19datahub.io/data-2.csv"
   x   <- read.csv(url)
   
   expect_equal(max(x$date) == Sys.Date(), TRUE)
@@ -18,9 +18,37 @@ test_that("server-2", {
 
 test_that("server-3", {
   
+  url <- "https://storage.covid19datahub.io/data-3.csv"
+  x   <- read.csv(url)
+  
+  expect_equal(max(x$date) >= Sys.Date()-1, TRUE)
+  
+})
+
+test_that("server-raw-1", {
+
+  url <- "https://storage.covid19datahub.io/rawdata-1.csv"
+  x   <- read.csv(url)
+  
+  expect_equal(max(x$date) == Sys.Date(), TRUE)
+  
+})
+
+test_that("server-raw-2", {
+  
+  url <- "https://storage.covid19datahub.io/rawdata-2.csv"
+  x   <- read.csv(url)
+  
+  expect_equal(max(x$date) == Sys.Date(), TRUE)
+  
+})
+
+test_that("server-raw-3", {
+  
   url <- "https://storage.covid19datahub.io/rawdata-3.csv"
   x   <- read.csv(url)
   
   expect_equal(max(x$date) >= Sys.Date()-1, TRUE)
   
 })
+
