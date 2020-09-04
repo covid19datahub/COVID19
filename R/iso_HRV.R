e386c785f84ef0f3c0ad02260118b155256c3457
Martin Beneš <martinbenes1996@gmail.com>
2020-09-04 18:44:48 +0200

    croatia confirmed+deaths level 2 implemented

---
+++ R/iso_HRV.R
@@ -0,0 +1,20 @@
+HRV <- function(level, cache) {
+  # Author: Martin Benes
+  
+  # fallback
+  if(level > 2)
+    return(NULL)
+  
+  # download
+  x <- gov_hr(level = level, cache = cache)
+  
+  # id
+  if(level == 2) {
+    x$id <- id(x$nuts, iso = "HRV", ds = "gov_hr", level = level)
+    
+    
+  }
+  # return
+  return(x)
+  
+}
