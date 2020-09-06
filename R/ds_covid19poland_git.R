a3870afeccb8c039389e420660c5763049b46ae6
Martin Beneš <martinbenes1996@gmail.com>
2020-09-06 22:44:06 +0200

    test numbers added

--- R/ds_covid19poland_git.R
+++ R/ds_covid19poland_git.R
@@ -1,21 +1,35 @@
 covid19poland_git <- function(level, cache){
   
   # source
-  repo    <- "https://raw.githubusercontent.com/martinbenes1996/covid19poland/master/data/"
-  url     <- "data.csv"
+  repo     <- "https://raw.githubusercontent.com/martinbenes1996/covid19poland/master/data/"
+  url      <- "data.csv"
+  test.url <- "tests.csv"
   
   # download
-  url <- sprintf("%s/%s", repo, url)
-  x   <- read.csv(url, cache = cache)
+  url      <- sprintf("%s/%s", repo, url)
+  test.url <- sprintf("%s/%s", repo, test.url)
+  x        <- read.csv(url, cache = cache)
+  x.test   <- read.csv(test.url, cache = cache)
   
   # format
-  x$date <- as.Date(x$date)
-  x <- x <- map_data(x, c(
+  x$date      <- as.Date(x$date)
+  x <- map_data(x, c(
     "date"  = "date",
     "NUTS2" = "state",
     "NUTS3" = "district"
   ))
   x <- x %>% dplyr::arrange(date)
+  # format test
+  x.test$date <- as.Date(x.test$date)
+  x.test <- map_data(x.test, c(
+    "date"   = "date",
+    "region" = "state",
+    "tests"  = "tests"
+  ))
+  x.test <- x.test %>%
+    dplyr::filter(!is.na(state)) %>%
+    dplyr::arrange(date, state)
+    
   
   # group
   if(level == 1) {
@@ -32,6 +46,10 @@ covid19poland_git <- function(level, cache){
       dplyr::group_by(state) %>%
       dplyr::arrange(date) %>%
       dplyr::mutate(deaths = cumsum(deaths))
+    
+    x <- x %>%
+      dplyr::full_join(x.test, by=c("date","state")) %>%
+      dplyr::arrange(date, state)
   }
   if(level == 3) {
     x <- x %>%
