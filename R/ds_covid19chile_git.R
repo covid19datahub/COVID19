0bd4dc395be99c00e17c6eb9e58f9ed3bd0e4148
emanuele-guidotti <emanuele.guidotti@hotmail.it>
2020-09-21 20:37:37 +0200

    fix CHL

--- R/ds_covid19chile_git.R
+++ R/ds_covid19chile_git.R
@@ -4,100 +4,112 @@ covid19chile_git <- function(level, cache) {
     
     # url
     url           <- "https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto5/TotalesNacionales_T.csv"
-    url.icu       <- "https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto9/HospitalizadosUCIEtario_T.csv"
-    url.crit      <- "https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto23/PacientesCriticos_T.csv"
+    url.hosp      <- "https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto24/CamasHospital_Diario_T.csv"
+    url.recovered <- "https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto46/activos_vs_recuperados.csv"
+    url.test      <- "https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto17/PCREstablecimiento_std.csv"
+    
     # download
     x           <- read.csv(url, cache = cache)
-    x.icu       <- read.csv(url.icu, cache = cache)
-    x.crit      <- read.csv(url.crit, cache = cache)
+    x.hosp      <- read.csv(url.hosp, cache = cache)
     x.recovered <- read.csv(url.recovered, cache = cache)
+    x.test      <- read.csv(url.test, cache = cache)
     
     # parse
     x <- map_data(x, c(
-      "Fecha"             = "date",
-      "Casos.totales"     = "confirmed",
-      "Casos.recuperados" = "recovered",
-      "Fallecidos"        = "deaths",
-      "Casos.activos"     = "active"))
+      "Fecha"                    = "date",
+      "Casos.totales"            = "confirmed",
+      "Fallecidos"               = "deaths",
+      "Casos.activos"            = "active"))
     
-    # parse icu
-    x <- x.icu %>%
-      dplyr::mutate(
-        date = Grupo.de.edad,
-        icu  = X..39 + X40.49 + X50.59 + X60.69 + X..70) %>%
-      dplyr::select(date, icu) %>%
-      dplyr::full_join(x, by = "date")
-    map_data(x.crit, c(
-      "Casos"              = "date",
-      "Pacientes.criticos" = "critical")) %>%
-      dplyr::full_join(x, by = "date")
-      
+    # parse hosp
+    x.hosp$hosp <- x.hosp$Basica + x.hosp$Media + x.hosp$UTI + x.hosp$UCI
+    x.hosp <- map_data(x.hosp, c(
+      "Tipo.de.cama" = "date",
+      "hosp"         = "hosp",
+      "UCI"          = "icu"
+    ))
+    
+    # parse recovered
+    x.recovered <- map_data(x.recovered, c(
+      "fecha_primeros_sintomas" = "date",
+      "recuperados"             = "recovered"
+    ))
+    
+    # parse test
+    x.test <- x.test[x.test$Establecimiento=="Total realizados",]
+    x.test <- map_data(x.test, c(
+      "fecha"         = "date",
+      "Numero.de.PCR" = "tests"
+    ))
+    
+    # merge
+    x <- x %>%
+      merge(x.hosp, by = "date", all = TRUE) %>%
+      merge(x.recovered, by = "date", all = TRUE) %>%
+      merge(x.test, by = "date", all = TRUE)
+    
+    # date
+    x$date <- as.Date(x$date, "%Y-%m-%d")
+    
   }
   
   if(level == 2) {
     
     # url
-    url      <- "https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto3/TotalesPorRegion_T.csv"
-    url.icu  <- "https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto8/UCI_T.csv"
-    url.test <- "https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto7/PCR_T.csv"
+    url      <- "https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto3/TotalesPorRegion.csv"
+    url.icu  <- "https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto8/UCI.csv"
+    url.test <- "https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto7/PCR.csv"
+    
     # download
     x      <- read.csv(url, cache = cache)
     x.icu  <- read.csv(url.icu, cache = cache)
     x.test <- read.csv(url.test, cache = cache)
     
-    # to mutate the region names
-    mutate_region <- function(col) {
-      # parse
-      col = sub('^(.*)\\.[0-9]$', '\\1', col)
-      col = gsub('\\.', ' ', col)
-      col = ifelse(col == "Araucania", "Araucanía", col)
-      # return
-      return(col)
-    }
+    # sanitize Araucania
+    x$Region      <- gsub("^Araucan.a$", "Araucania", x$Region)
+    x.icu$Region  <- gsub("^Araucan.a$", "Araucania", x.icu$Region)
+    x.test$Region <- gsub("^Araucan.a$", "Araucania", x.test$Region)
     
-    transform.column <- function(x, value) 
-      x[c(-1,-2),] %>%
-        dplyr::rename(date = Region) %>%
-        # longer
-        tidyr::pivot_longer(!date, names_to = "region", values_to = value) %>%
-        dplyr::mutate(region = mutate_region(region))
-        
-    x.test <- transform.column(x.test, "tests")
-    x.icu  <- transform.column(x.icu, "icu")
+    # sanitize O'Higgins
+    x$Region      <- gsub("^O.Higgins$", "O'Higgins", x$Region)
+    x.icu$Region  <- gsub("^O.Higgins$", "O'Higgins", x.icu$Region)
+    x.test$Region <- gsub("^O.Higgins$", "O'Higgins", x.test$Region)
+
+    # formatting x
+    x$Categoria <- map_values(x$Categoria, c(
+      "Casos acumulados"              = "confirmed",
+      "Fallecidos totales"            = "deaths",
+      "Casos confirmados recuperados" = "recovered"
+    ))
     
-    # join headers
-    colnames(x) <- c("date", paste(colnames(x[,-1]), x[1,-1], sep = "_"))
-    x <- x[-1,] %>%
-      # longer
-      tidyr::pivot_longer(!date, names_to = "name", values_to = "value") %>%
-      # split into multiple columns
-      dplyr::mutate(
-        region = sapply(strsplit(name, "_"), "[", 1),
-        item   = sapply(strsplit(name, "_"), "[", 2),
-        value  = as.numeric(value)) %>%
-      dplyr::mutate(region = mutate_region(region)) %>%
-      dplyr::select(date, value, region, item) %>%
-      # create data columns
-      tidyr::pivot_wider(names_from = item, values_from = value) %>%
-      # rename columns
-      map_data(c(
-        "date"                          = "date",
-        "region"                        = "region",
-        "Casos acumulados"              = "confirmed",
-        "Fallecidos totales"            = "deaths",
-        "Casos confirmados recuperados" = "recovered")) #%>%
-      #dplyr::group_by(region) %>%
-      #dplyr::arrange(as.Date(date,"%Y-%m-%d")) %>%
-      #tidyr::fill(deaths, .direction = "up")
-    
-    # join with tests
     x <- x %>%
-      dplyr::filter(region != "Total") %>%
-      #dplyr::full_join(x.test, by = c("date","region")) %>% # error in data (too low in comparison to confirmed)
-      dplyr::full_join(x.icu, by = c("date","region"))
+      tidyr::pivot_longer(cols = -c("Region", "Categoria"), names_to = 'date', values_to = 'value') %>%
+      tidyr::pivot_wider(names_from = "Categoria", values_from = "value")
     
-    # sanitize
-    x$region <- trimws(x$region)
+    # formatting x.icu
+    x.icu <- x.icu %>% 
+      tidyr::pivot_longer(cols = -c("Region", "Codigo.region", "Poblacion"), names_to = "date", values_to = "icu")
+    
+    # formatting tests
+    x.test <- x.test %>% 
+      tidyr::pivot_longer(cols = -c("Region", "Codigo.region", "Poblacion"), names_to = "date", values_to = "tests") %>%
+      dplyr::group_by_at("Region") %>%
+      dplyr::arrange_at("date") %>%
+      dplyr::mutate(tests = cumsum(tests, na.rm = TRUE))
+    
+    # merge
+    x <- x[x$Region!="Total",] %>%
+      merge(x.icu, by = c("date","Region"), all = TRUE) %>%
+      merge(x.test, by = c("date","Region"), all = TRUE)
+    
+    # fix missing codes after merge
+    map <- x$Codigo.region
+    names(map) <- x$Region
+    x$Codigo.region <- map_values(x$Region, map)
+    
+    # date
+    x$date <- as.Date(x$date, format = "X%Y.%m.%d")
+    
   }
   
   if(level == 3) {
@@ -105,37 +117,33 @@ covid19chile_git <- function(level, cache) {
     # url
     url        <- "https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto1/Covid-19.csv"
     url.deaths <- "https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto38/CasosFallecidosPorComuna.csv"
+    
     # download
     x        <- read.csv(url, cache = cache)
     x.deaths <- read.csv(url.deaths, cache = cache)
     
-    # to transform data
-    transform.date.columns <- function(x, value) x %>%
-        dplyr::rename(region = Region, commune = Comuna, population = Poblacion) %>%
-        # longer dates
-        dplyr::select(-c(Codigo.region, Codigo.comuna)) %>%
-        tidyr::pivot_longer(-c(region,commune,population),
-                            names_prefix = "X", names_to = "date",
-                            values_to = value) %>%
-        dplyr::mutate(date = gsub("\\.", "-", date))
+    # by
+    by <- c("Region", "Codigo.region", "Comuna", "Codigo.comuna", "Poblacion")
     
-    # transform date columns
-    x        <- transform.date.columns(x %>% dplyr::select(-c(Tasa)), "confirmed")
-    x.deaths <- transform.date.columns(x.deaths, "deaths")
-
-    # join
-    x <- x %>%
-      dplyr::full_join(x.deaths, by = c("date","region","commune","population")) %>%
-      dplyr::filter(!grepl("Desconocido", commune)) %>%
-      dplyr::filter(commune != "Total") 
+    # formatting x
+    x <- x[,colnames(x)!="Tasa"] %>% 
+      tidyr::pivot_longer(cols = -dplyr::all_of(by), names_to = "date", values_to = "confirmed")
     
-    # sanitize
-    x$commune <- trimws(x$commune)
+    # formatting x.deaths
+    x.deaths <- x.deaths %>% 
+      tidyr::pivot_longer(cols = -dplyr::all_of(by), names_to = "date", values_to = "deaths")
+   
+    # merge
+    x <- x[,c("date", "Codigo.comuna", "confirmed")] %>%
+      merge(x.deaths, by = c("date", "Codigo.comuna"), all = TRUE)
+     
+    # drop total
+    x <- x[!is.na(x$Codigo.comuna),]
+    
+    # date
+    x$date <- as.Date(x$date, format = "X%Y.%m.%d")
     
   }
-  
-  # date
-  x$date <- as.Date(x$date, "%Y-%m-%d")
   
   # return
   return(x)
--- R/iso_CHL.R
+++ R/iso_CHL.R
@@ -1,16 +1,17 @@
 CHL <- function(level, cache) {
   
   # fallback
-  if(level == 1 | level > 3) return(NULL)
+  if(level > 3) 
+    return(NULL)
   
   # download
   x <- covid19chile_git(level = level, cache = cache)
   
   # id
   if(level == 2)
-    x$id <- id(paste("region", x$region, sep="_"), iso = "CHL", ds = "covid19chile_git", level = level)
+    x$id <- id(x$Codigo.region, iso = "CHL", ds = "covid19chile_git", level = level)
   if(level == 3)
-    x$id <- id(x$commune, iso = "CHL", ds = "covid19chile_git", level = level)
+    x$id <- id(x$Codigo.comuna, iso = "CHL", ds = "covid19chile_git", level = level)
   
   # return
   return(x)
