879445da97822edc4c787adb4ab4ec268f5f95c5
emanuele-guidotti <emanuele.guidotti@hotmail.it>
2021-03-01 19:41:09 +0100

    Update testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,14 +3,13 @@
 # Install this package and run this script based on
 # https://testthat.r-lib.org/
 #
-# This test script:
-# - prints changes in the data with respect to the (cloud) snapshot of 1 hour ago (live data) and 2 days ago (vintage data)
-# - raises warnings if data providers made changes requiring manual fix
-# - raises errors if data providers made breaking changes (test failure)
-#
 # Additional information on testing R packages
 # http://r-pkgs.had.co.nz/tests.html
 
 library("testthat")
 
+# download timeout for large files
+options("timeout" = 60*60)
+
+# run tests
 test_check("COVID19")
