19fa9931bf55a4cbd2fe8edc0a5429124c8d989e
emanuele-guidotti <emanuele.guidotti@hotmail.it>
2021-03-02 00:31:37 +0100

    Update test-levels.R

--- tests/testthat/test-levels.R
+++ tests/testthat/test-levels.R
@@ -18,10 +18,20 @@ test_that("level-2", {
   
 })
 
-test_that("level-3", {
+test_that("level-3-ITA", {
   
   # try 
-  covid19(level = 3, raw = TRUE, vintage = FALSE, verbose = FALSE, debug = TRUE)
+  covid19("ITA", level = 3, raw = TRUE, vintage = FALSE, verbose = FALSE, debug = TRUE)
+  
+  # success
+  return(TRUE)
+  
+})
+
+test_that("level-3-USA", {
+  
+  # try 
+  covid19("USA", level = 3, raw = TRUE, vintage = FALSE, verbose = FALSE, debug = TRUE)
   
   # success
   return(TRUE)
