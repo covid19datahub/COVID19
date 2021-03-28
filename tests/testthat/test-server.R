5a1989df9087bd34dbb3a2a24f5d0cbd57b0e9f5
emanuele-guidotti <emanuele.guidotti@hotmail.it>
2021-03-28 22:54:03 +0200

    Update test-server.R

--- tests/testthat/test-server.R
+++ tests/testthat/test-server.R
@@ -16,14 +16,14 @@ test_that("server-2", {
   
 })
 
-test_that("server-3", {
-  
-  url <- "https://storage.covid19datahub.io/data-3.csv"
-  x   <- read.csv(url)
-  
-  expect_equal(max(x$date) >= Sys.Date()-1, TRUE)
-  
-})
+# test_that("server-3", {
+#   
+#   url <- "https://storage.covid19datahub.io/data-3.csv"
+#   x   <- read.csv(url)
+#   
+#   expect_equal(max(x$date) >= Sys.Date()-1, TRUE)
+#   
+# })
 
 test_that("server-raw-1", {
 
