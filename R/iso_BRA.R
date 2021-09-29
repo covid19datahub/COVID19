66bd243bffdfd94b4d5d36876cc187c50fa72c3a
emanuele-guidotti <emanuele.guidotti@hotmail.it>
2021-09-29 23:02:53 +0200

    Fix BRA

--- R/iso_BRA.R
+++ R/iso_BRA.R
@@ -7,9 +7,10 @@ BRA <- function(level, cache) {
   
   if(level==3){  
     
-    # download Espirito Santo
-    y <- gov_br_es(level = level, cache = cache)
-    y$id <- id(y$Municipio, iso = "BRA", ds = "gov_br_es", level = level)
+    # Espirito Santo (additional data for tests and recovered)
+    # Check agreement with https://coronavirus.es.gov.br/painel-covid-19-es
+    # y <- gov_br_es(level = level, cache = cache)
+    # y$id <- id(y$Municipio, iso = "BRA", ds = "gov_br_es", level = level)
 
     # vaccines
     v <- gov_br(level = level, cache = cache)
@@ -18,8 +19,8 @@ BRA <- function(level, cache) {
     # merge
     x$id <- id(x$code, iso = "BRA", ds = "covid19br_git", level = level)
     x <- x %>%
-      dplyr::filter(state != "ES") %>%
-      dplyr::bind_rows(y) %>%
+      # dplyr::filter(state != "ES") %>%
+      # dplyr::bind_rows(y) %>%
       dplyr::left_join(v, by = c("id", "date"))
     
   }
