2ae25c5b821b17e5c314083bc7fe0644de6a9d12
emanuele-guidotti <emanuele.guidotti@hotmail.it>
2021-10-25 16:21:11 +0200

    Update iso_BES.R

--- R/iso_BES.R
+++ R/iso_BES.R
@@ -1,8 +1,39 @@
-BES <- function(level, ...){
-  if(level>1) return(NULL)
+#' Bonaire, Sint Eustatius and Saba
+#'
+#' @source \url{`r repo("BES")`}
+#' 
+BES <- function(level){
+  x <- NULL
   
-  x <- github.cssegisanddata.covid19(file = "global", cache = TRUE, level = 2, state = "Bonaire, Sint Eustatius and Saba")
+  #' @concept Level 1
+  #' @section Data Sources:
+  #' 
+  #' ## Level 1
+  #' `r docstring("BES", 1)`
+  #' 
+  if(level==1){
+    
+    #' - \href{`r repo("github.cssegisanddata.covid19")`}{Johns Hopkins Center for Systems Science and Engineering}:
+    #' confirmed cases,
+    #' deaths,
+    #' recovered.
+    #'
+    x1 <- github.cssegisanddata.covid19(state = "Bonaire, Sint Eustatius and Saba", level = 2)
+    
+    #' - \href{`r repo("ourworldindata.org")`}{Our World in Data}:
+    #' tests,
+    #' total vaccine doses administered,
+    #' people with at least one vaccine dose,
+    #' people fully vaccinated,
+    #' hospitalizations,
+    #' intensive care.
+    #'
+    x2 <- ourworldindata.org(id = "BES")
+    
+    # merge
+    x <- full_join(x1, x2, by = "date")
+    
+  }
   
   return(x)
-  
 }
