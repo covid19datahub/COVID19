11b4a8e5a8dba0ae4f2646511482a5904e06f694
emanuele-guidotti <emanuele.guidotti@hotmail.it>
2021-10-30 22:04:14 +0200

    Update GLP

--- R/iso_GLP.R
+++ R/iso_GLP.R
@@ -1,25 +1,40 @@
 #' Guadeloupe
+#'
+#' @source \url{`r repo("GLP")`}
 #' 
-#' Data available at level 1 (nation).
-#' 
-#' @section Data sources:
-#' 
-#' \bold{Level 1.}
-#' \href{`r repo("gouv.fr")`}{Santé publique France}
-#' (confirmed cases, deaths, tests, hospitalizations, vaccines); 
-#' \href{https://data.worldbank.org/indicator/SP.POP.TOTL}{World Bank Open Data}
-#' (population 2018).
-#' 
-#' @source `r repo("GLP")`
-#' 
-#' @concept level 1
-#' 
-GLP <- function(level, ...){
-  if(level>1) return(NULL)
+GLP <- function(level){
+  x <- NULL
   
-  # confirmed, deaths, tests, hospitalizations, vaccines
-  x <- gouv.fr(level = 3, dep = "971")  
+  #' @concept Level 1
+  #' @section Data Sources:
+  #' 
+  #' ## Level 1
+  #' `r docstring("GLP", 1)`
+  #' 
+  if(level==1){
+    
+    #' - \href{`r repo("gouv.fr")`}{Santé Publique France}:
+    #' confirmed cases,
+    #' deaths,
+    #' tests,
+    #' total vaccine doses administered,
+    #' people with at least one vaccine dose,
+    #' people fully vaccinated,
+    #' hospitalizations,
+    #' intensive care.
+    #'
+    x1 <- gouv.fr(dep = "971", level = 3)
+    
+    #' - \href{`r repo("github.cssegisanddata.covid19")`}{Johns Hopkins Center for Systems Science and Engineering}:
+    #' recovered.
+    #'
+    x2 <- github.cssegisanddata.covid19(country = "Guadeloupe") %>%
+      select(-c("confirmed", "deaths"))
+    
+    # merge
+    x <- full_join(x1, x2, by = "date")
+    
+  }
   
   return(x)
-  
 }
