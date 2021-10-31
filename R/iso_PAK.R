b8ee4374a8ac5d707ff303cafd54f115716714a6
emanuele-guidotti <emanuele.guidotti@hotmail.it>
2021-10-31 16:30:19 +0100

    Update PAK

--- R/iso_PAK.R
+++ R/iso_PAK.R
@@ -1,18 +1,56 @@
-PAK <- function(level, ...){
+#' Pakistan
+#'
+#' @source \url{`r repo("PAK")`}
+#' 
+PAK <- function(level){
+  x <- NULL
   
-  # fallback
-  if(level>2)
-    return(NULL)
+  #' @concept Level 1
+  #' @section Data Sources:
+  #' 
+  #' ## Level 1
+  #' `r docstring("PAK", 1)`
+  #' 
+  if(level==1){
+    
+    #' - \href{`r repo("github.cssegisanddata.covid19")`}{Johns Hopkins Center for Systems Science and Engineering}:
+    #' confirmed cases,
+    #' deaths,
+    #' recovered.
+    #'
+    x1 <- github.cssegisanddata.covid19(country = "Pakistan")
+    
+    #' - \href{`r repo("ourworldindata.org")`}{Our World in Data}:
+    #' tests,
+    #' total vaccine doses administered,
+    #' people with at least one vaccine dose,
+    #' people fully vaccinated,
+    #' hospitalizations,
+    #' intensive care.
+    #'
+    x2 <- ourworldindata.org(id = "PAK")
+    
+    # merge
+    x <- full_join(x1, x2, by = "date")
+    
+  }
   
-  # fallback to worldwide data
-  if(level==1)
-    x <- NULL
-  
-  # download already with ids
-  if(level==2)
+  #' @concept Level 2
+  #' @section Data Sources:
+  #' 
+  #' ## Level 2
+  #' `r docstring("PAK", 2)`
+  #' 
+  if(level==2){
+    
+    #' - \href{`r repo("github.cssegisanddata.covid19unified")`}{Johns Hopkins Center for Systems Science and Engineering}:
+    #' confirmed cases,
+    #' deaths,
+    #' recovered.
+    #'
     x <- github.cssegisanddata.covid19unified(iso = "PAK", level = level)
+    
+  }
   
-  # return
   return(x)
-  
 }
