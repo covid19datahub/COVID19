5ebe1cce5333f144530b33a6018bfa0a0f1d8c73
emanuele-guidotti <emanuele.guidotti@hotmail.it>
2021-10-31 23:03:56 +0100

    Update TWN

---
+++ R/ds_gov.tw.R
@@ -0,0 +1,90 @@
+#' Ministry of Health and Welfare of Taiwan
+#'
+#' Data source for: Taiwan
+#'
+#' @param level 1, 2
+#'
+#' @section Level 1:
+#' - confirmed cases
+#' - tests
+#'
+#' @section Level 2:
+#' - confirmed cases
+#'
+#' @source https://data.gov.tw
+#'
+#' @keywords internal
+#'
+gov.tw <- function(level) {
+  if(!level %in% 1:2) return(NULL)
+
+  # download cases
+  url <- "https://od.cdc.gov.tw/eic/Day_Confirmation_Age_County_Gender_19CoV.csv"
+  x   <- read.csv(url, encoding = "UTF-8")
+  
+  # format cases
+  x <- x[,-1]
+  colnames(x) <- c("date", "county", "city", "gender", "imported", "age_group", "confirmed")
+  
+  # convert date
+  x$date <- as.Date(x$date, "%Y/%m/%d")
+
+  if(level == 1) {
+    
+    # download tests
+    # see https://data.gov.tw/dataset/120451
+    url.tests <- "https://od.cdc.gov.tw/eic/covid19/covid19_tw_specimen.csv"
+    x.tests   <- read.csv(url.tests, encoding = "UTF-8")
+    
+    # format tests
+    colnames(x.tests) <- c("date", "notification", "home quarantine", "monitoring", "total")
+    x.tests <- x.tests %>%
+      # convert date
+      mutate(date  = as.Date(date, "%Y/%m/%d")) %>%
+      # sort by date
+      arrange(date) %>%
+      # cumulate
+      mutate(tests = cumsum(total)) %>%
+      # subset
+      select(date, tests) %>%
+      # drop missing values
+      filter(!is.na(tests))
+    
+    # cases
+    x <- x %>% 
+      # for each date
+      group_by(date) %>%
+      # compute total counts
+      summarise(confirmed = sum(confirmed)) %>%
+      # sort by date
+      arrange(date) %>%
+      # cumulate
+      mutate(confirmed = cumsum(confirmed))
+      
+    # merge
+    x <- full_join(x, x.tests, by = "date")
+    
+  }
+  
+  if(level == 2) {
+    
+    # drop unassigned
+    x <- filter(x, x$county!="空值")
+
+    # cases
+    x <- x %>% 
+      # for each date and countr
+      group_by(date, county) %>%
+      # compute total counts
+      summarise(confirmed = sum(confirmed)) %>%
+      # group by county
+      group_by(county) %>%
+      # sort by date
+      arrange(date) %>%
+      # cumulate
+      mutate(confirmed = cumsum(confirmed))
+    
+  }
+    
+  return(x)
+}
--- R/iso_TWN.R
+++ R/iso_TWN.R
@@ -1,17 +1,63 @@
-TWN <- function(level, cache) {
-  # Author: Lim Jone Keat
+#' Taiwan
+#'
+#' @source \url{`r repo("TWN")`}
+#' 
+TWN <- function(level){
+  x <- NULL
   
-  # fallback
-  if(level > 2)
-    return(NULL)
+  #' @concept Level 1
+  #' @section Data Sources:
+  #' 
+  #' ## Level 1
+  #' `r docstring("TWN", 1)`
+  #' 
+  if(level==1){
+    
+    #' - \href{`r repo("gov.tw")`}{Ministry of Health and Welfare of Taiwan}:
+    #' confirmed cases,
+    #' tests.
+    #'
+    x1 <- gov.tw(level = level)
+    
+    #' - \href{`r repo("github.cssegisanddata.covid19")`}{Johns Hopkins Center for Systems Science and Engineering}:
+    #' deaths,
+    #' recovered.
+    #'
+    x2 <- github.cssegisanddata.covid19(country = "Taiwan*") %>%
+      select(-c("confirmed"))
+    
+    #' - \href{`r repo("ourworldindata.org")`}{Our World in Data}:
+    #' total vaccine doses administered,
+    #' people with at least one vaccine dose,
+    #' people fully vaccinated,
+    #' hospitalizations,
+    #' intensive care.
+    #'
+    x3 <- ourworldindata.org(id = "TWN") %>%
+      select(-c("tests"))
+    
+    # merge
+    x <- x1 %>%
+      full_join(x2, by = "date") %>%
+      full_join(x3, by = "date")
+    
+  }
   
-  # download
-  x <- gov_tw(level = level, cache = cache)
+  #' @concept Level 2
+  #' @section Data Sources:
+  #' 
+  #' ## Level 2
+  #' `r docstring("TWN", 2)`
+  #' 
+  if(level==2){
+    
+    #' - \href{`r repo("gov.tw")`}{Ministry of Health and Welfare of Taiwan}:
+    #' confirmed cases.
+    #'
+    x <- gov.tw(level = level)
+    x$id <- id(x$county, iso = "TWN", ds = "gov.tw", level = level)
+    
+  }
   
-  # level
-  if(level==2)
-    x$id <- id(x$county, iso = "TWN", ds = "gov_tw", level = level)
-  
-  # return
   return(x)
 }
