e9286d5ea4b737ae98f5ea48125777fccfab5a71
emanuele-guidotti <emanuele.guidotti@hotmail.it>
2021-10-31 12:40:20 +0100

    Update LIE

--- R/iso_LIE.R
+++ R/iso_LIE.R
@@ -1,19 +1,41 @@
-LIE <- function(level, cache){
-
-  # fallback
-  if(level>1)
-    return(NULL)
-
-  # download
-  x <- admin.ch(id = "FL", level = 1)
-  y <- github.openzh.covid19(id = "FL", cache = cache)
+#' Liechtenstein
+#'
+#' @source \url{`r repo("LIE")`}
+#' 
+LIE <- function(level){
+  x <- NULL
   
-  # merge
-  y <- y[,c("date", "hosp", "recovered")]
-  x <- merge(x, y, by = c("date"), all = TRUE)
+  #' @concept Level 1
+  #' @section Data Sources:
+  #' 
+  #' ## Level 1
+  #' `r docstring("LIE", 1)`
+  #' 
+  if(level==1){
+    
+    #' - \href{`r repo("admin.ch")`}{Federal Office of Public Health}:
+    #' confirmed cases,
+    #' deaths,
+    #' tests,
+    #' total vaccine doses administered,
+    #' people with at least one vaccine dose,
+    #' people fully vaccinated,
+    #' hospitalizations,
+    #' intensive care.
+    #'
+    x1 <- admin.ch(state = "FL", level = 1)
+    
+    #' - \href{`r repo("github.openzh.covid19")`}{Specialist Unit for Open Government Data Canton of Zurich}:
+    #' recovered,
+    #' patients requiring ventilation.
+    #'
+    x2 <- github.openzh.covid19(state = "FL", level = 1) %>%
+      select(-c("confirmed", "deaths", "tests", "hosp", "icu"))
+    
+    # merge
+    x <- merge(x1, x2, by = "date")
+    
+  }
   
-  # return
   return(x)
-
 }
-
