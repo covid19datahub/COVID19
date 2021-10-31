8c0dab01b68ccb95d9ce4efa6bc8b4de2d8a62dc
emanuele-guidotti <emanuele.guidotti@hotmail.it>
2021-10-31 23:08:42 +0100

    Update UKR

--- R/iso_UKR.R
+++ R/iso_UKR.R
@@ -1,18 +1,56 @@
-UKR <- function(level, ...){
+#' Ukraine
+#'
+#' @source \url{`r repo("UKR")`}
+#' 
+UKR <- function(level){
+  x <- NULL
   
-  # fallback
-  if(level>2)
-    return(NULL)
+  #' @concept Level 1
+  #' @section Data Sources:
+  #' 
+  #' ## Level 1
+  #' `r docstring("UKR", 1)`
+  #' 
+  if(level==1){
+    
+    #' - \href{`r repo("github.cssegisanddata.covid19")`}{Johns Hopkins Center for Systems Science and Engineering}:
+    #' confirmed cases,
+    #' deaths,
+    #' recovered.
+    #'
+    x1 <- github.cssegisanddata.covid19(country = "Ukraine")
+    
+    #' - \href{`r repo("ourworldindata.org")`}{Our World in Data}:
+    #' tests,
+    #' total vaccine doses administered,
+    #' people with at least one vaccine dose,
+    #' people fully vaccinated,
+    #' hospitalizations,
+    #' intensive care.
+    #'
+    x2 <- ourworldindata.org(id = "UKR")
+    
+    # merge
+    x <- full_join(x1, x2, by = "date")
+    
+  }
   
-  # fallback to worldwide data
-  if(level==1)
-    x <- NULL
-  
-  # download already with ids
-  if(level==2)
+  #' @concept Level 2
+  #' @section Data Sources:
+  #' 
+  #' ## Level 2
+  #' `r docstring("UKR", 2)`
+  #' 
+  if(level==2){
+    
+    #' - \href{`r repo("github.cssegisanddata.covid19unified")`}{Johns Hopkins Center for Systems Science and Engineering}:
+    #' confirmed cases,
+    #' deaths,
+    #' recovered.
+    #'
     x <- github.cssegisanddata.covid19unified(iso = "UKR", level = level)
+    
+  }
   
-  # return
   return(x)
-  
 }
