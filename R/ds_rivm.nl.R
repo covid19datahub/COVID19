d6fa71aad5806ef77960050889741e85fd6aaac2
emanuele-guidotti <emanuele.guidotti@hotmail.it>
2021-10-31 16:20:54 +0100

    Update NLD

--- R/ds_rivm.nl.R
+++ R/ds_rivm.nl.R
@@ -1,25 +1,47 @@
-rivm_nl <- function(level) {
-  
+#' National Institute for Public Health and the Environment
+#'
+#' Data source for: Netherlands
+#'
+#' @param level 1, 2, 3
+#'
+#' @section Level 1:
+#' - confirmed cases
+#' - deaths
+#'
+#' @section Level 2:
+#' - confirmed cases
+#' - deaths
+#'
+#' @section Level 3:
+#' - confirmed cases
+#' - deaths
+#'
+#' @source https://data.rivm.nl/covid-19/
+#'
+#' @keywords internal
+#'
+rivm.nl <- function(level) {
+  if(!level %in% 1:3) return(NULL)
+
   # download
   url <- "https://data.rivm.nl/covid-19/COVID-19_aantallen_gemeente_per_dag.csv"
   x   <- read.csv(url, sep = ";")
   
-  # parse
+  # format
   x <- map_data(x, c(
     "Date_of_publication" = "date",
     "Municipality_code"   = "municipality_code",
     "Municipality_name"   = "municipality",
     "Province"            = "province",
     "Total_reported"      = "confirmed",
-    "Hospital_admission"  = "hosp", 
     "Deceased"            = "deaths")) 
   
   # sanitize
   x$date <- as.Date(x$date)
   x$province <- trimws(x$province)
   x$municipality <- trimws(x$municipality)
-
-  # level
+  
+  # group by
   if(level == 1){
     by <- c("date") 
   }
@@ -32,18 +54,22 @@ rivm_nl <- function(level) {
     x <- x[!is.na(x$province) & !is.na(x$municipality),]
   }
   
-  # group and cumulate
+  # aggregate
   x <- x %>% 
+    # for each date and area
     dplyr::group_by_at(by) %>%
-    dplyr::summarise(confirmed = sum(confirmed),
-                     deaths    = sum(deaths),
-                     hosp      = sum(hosp)) %>%
+    # compute total counts
+    dplyr::summarise(
+      confirmed = sum(confirmed),
+      deaths    = sum(deaths)) %>%
+    # group by area
     dplyr::group_by_at(by[-1]) %>%
+    # sort by date
     dplyr::arrange(date) %>%
-    dplyr::mutate(confirmed = cumsum(confirmed),
-                  deaths    = cumsum(deaths))
+    # cumulate
+    dplyr::mutate(
+      confirmed = cumsum(confirmed),
+      deaths    = cumsum(deaths))
   
-  # return
   return(x)
-  
 }
--- R/iso_NLD.R
+++ R/iso_NLD.R
@@ -1,18 +1,72 @@
-NLD <- function(level, ...) {
+#' Netherlands
+#'
+#' @source \url{`r repo("NLD")`}
+#' 
+NLD <- function(level){
+  x <- NULL
   
-  # fallback
-  if(level > 3) return(NULL)
+  #' @concept Level 1
+  #' @section Data Sources:
+  #' 
+  #' ## Level 1
+  #' `r docstring("NLD", 1)`
+  #' 
+  if(level==1){
+    
+    #' - \href{`r repo("rivm.nl")`}{National Institute for Public Health and the Environment}:
+    #' confirmed cases,
+    #' deaths.
+    #'
+    x1 <- rivm.nl(level = level)
+    
+    #' - \href{`r repo("ourworldindata.org")`}{Our World in Data}:
+    #' tests,
+    #' total vaccine doses administered,
+    #' people with at least one vaccine dose,
+    #' people fully vaccinated,
+    #' hospitalizations,
+    #' intensive care.
+    #'
+    x2 <- ourworldindata.org(id = "NLD")
+    
+    # merge
+    x <- full_join(x1, x2, by = "date")
+    
+  }
   
-  # download
-  x <- rivm_nl(level)
+  #' @concept Level 2
+  #' @section Data Sources:
+  #' 
+  #' ## Level 2
+  #' `r docstring("NLD", 2)`
+  #' 
+  if(level==2){
+    
+    #' - \href{`r repo("rivm.nl")`}{National Institute for Public Health and the Environment}:
+    #' confirmed cases,
+    #' deaths.
+    #'
+    x <- rivm.nl(level = level)
+    x$id <- id(x$province, iso = "NLD", ds = "rivm.nl", level = level)
+    
+  }
   
-  # id
-  if(level == 2)
-    x$id <- id(x$province, iso = "NLD", ds = "rivm_nl", level = level)
-  if(level == 3)
-    x$id <- id(x$municipality, iso = "NLD", ds = "rivm_nl", level = level)
+  #' @concept Level 3
+  #' @section Data Sources:
+  #' 
+  #' ## Level 3
+  #' `r docstring("NLD", 3)`
+  #' 
+  if(level==3){  
+    
+    #' - \href{`r repo("rivm.nl")`}{National Institute for Public Health and the Environment}:
+    #' confirmed cases,
+    #' deaths.
+    #'
+    x <- rivm.nl(level = level)
+    x$id <- id(x$municipality, iso = "NLD", ds = "rivm.nl", level = level)
+    
+  }
   
-  # return
   return(x)
-  
 }
