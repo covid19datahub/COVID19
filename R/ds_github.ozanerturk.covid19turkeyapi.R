a366bf035877ab5d3163ad004b2b1d8bc6d54752
emanuele-guidotti <emanuele.guidotti@hotmail.it>
2021-10-31 22:43:22 +0100

    Update TUR

--- R/ds_github.ozanerturk.covid19turkeyapi.R
+++ R/ds_github.ozanerturk.covid19turkeyapi.R
@@ -1,9 +1,27 @@
-covid19turkey_git <- function(level, cache){
-  # Author: Muhammed Seehan Pengatkundil
-  
+#' Ozan Erturk
+#'
+#' Data source for: Turkey
+#'
+#' @param level 1
+#'
+#' @section Level 1:
+#' - confirmed cases
+#' - deaths
+#' - recovered
+#' - tests
+#' - intensive care
+#' - patients requiring ventilation
+#'
+#' @source https://github.com/ozanerturk/covid19-turkey-api
+#'
+#' @keywords internal
+#'
+github.ozanerturk.covid19turkeyapi <- function(level){
+  if(level!=1) return(NULL)
+
   # download
   url <- "https://raw.githubusercontent.com/ozanerturk/covid19-turkey-api/master/dataset/timeline.csv"
-  x   <- read.csv(url, cache = cache, na.strings = c("", "-")) 
+  x   <- read.csv(url, na.strings = c("", "-")) 
   
   # formatting
   x <- map_data(x, c(
@@ -16,11 +34,9 @@ covid19turkey_git <- function(level, cache){
     'totalIntubated'     = 'vent'
   ))
   
-  # filter date
+  # clean date
   x <- x[!is.na(x$date),]
   x$date <- as.Date(x$date, format = "%d/%m/%Y")
   
-  # return
   return(x)
 }
-
--- R/iso_TUR.R
+++ R/iso_TUR.R
@@ -1,14 +1,48 @@
-TUR <- function(level, cache){
-  # Author: Muhammed Seehan Pengatkundil
+#' Turkey
+#'
+#' @source \url{`r repo("TUR")`}
+#' 
+TUR <- function(level){
+  x <- NULL
   
-  # fallback
-  if(level>1)
-    return(NULL)
+  #' @concept Level 1
+  #' @section Data Sources:
+  #' 
+  #' ## Level 1
+  #' `r docstring("TUR", 1)`
+  #' 
+  if(level==1){
+    
+    #' - \href{`r repo("github.cssegisanddata.covid19")`}{Johns Hopkins Center for Systems Science and Engineering}:
+    #' confirmed cases,
+    #' deaths,
+    #' recovered.
+    #'
+    x1 <- github.cssegisanddata.covid19(country = "Turkey")
+    
+    #' - \href{`r repo("github.ozanerturk.covid19turkeyapi")`}{Ozan Erturk}:
+    #' intensive care,
+    #' patients requiring ventilation.
+    #'
+    x2 <- github.ozanerturk.covid19turkeyapi(level = level) %>%
+      select(-c("confirmed", "deaths", "recovered", "tests"))
+    
+    #' - \href{`r repo("ourworldindata.org")`}{Our World in Data}:
+    #' tests,
+    #' total vaccine doses administered,
+    #' people with at least one vaccine dose,
+    #' people fully vaccinated,
+    #' hospitalizations.
+    #'
+    x3 <- ourworldindata.org(id = "TUR") %>%
+      select(-c("icu"))
+    
+    # merge
+    x <- x1 %>%
+      full_join(x2, by = "date") %>%
+      full_join(x3, by = "date")
+    
+  }
   
-  # download
-  x <- covid19turkey_git(level = level, cache = cache) 
-  
-  # return
   return(x)
-  
 }
