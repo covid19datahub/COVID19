4ce6cb721a982bd74d04303e5fbaee55f034f946
Emanuele Guidotti <emanuele.guidotti@unine.ch>
2022-10-07 17:19:23 +0200

    Fix DZA

--- R/iso_DZA.R
+++ R/iso_DZA.R
@@ -21,14 +21,14 @@ DZA <- function(level){
     x1 <- github.cssegisanddata.covid19(country = "Algeria")
     
     #' - \href{`r repo("ourworldindata.org")`}{Our World in Data}:
-    #' tests,
     #' total vaccine doses administered,
     #' people with at least one vaccine dose,
     #' people fully vaccinated,
     #' hospitalizations,
     #' intensive care.
     #'
-    x2 <- ourworldindata.org(id = "DZA")
+    x2 <- ourworldindata.org(id = "DZA") %>%
+      select(-c("tests"))
     
     # merge
     x <- full_join(x1, x2, by = "date")
