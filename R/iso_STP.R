1f507ed3208712e055ff903d51a3126773885aeb
Emanuele Guidotti <emanuele.guidotti@unine.ch>
2022-10-07 17:22:47 +0200

    Fix STP

--- R/iso_STP.R
+++ R/iso_STP.R
@@ -21,14 +21,14 @@ STP <- function(level){
     x1 <- github.cssegisanddata.covid19(country = "Sao Tome and Principe")
     
     #' - \href{`r repo("ourworldindata.org")`}{Our World in Data}:
-    #' tests,
     #' total vaccine doses administered,
     #' people with at least one vaccine dose,
     #' people fully vaccinated,
     #' hospitalizations,
     #' intensive care.
     #'
-    x2 <- ourworldindata.org(id = "STP")
+    x2 <- ourworldindata.org(id = "STP") %>%
+      select(-c("tests"))
     
     # merge
     x <- full_join(x1, x2, by = "date")
