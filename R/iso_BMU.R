07b052421694f8fda241c403a9f982f48ab2ef85
Emanuele Guidotti <emanuele.guidotti@unine.ch>
2022-10-07 17:13:09 +0200

    Fix BMU

--- R/iso_BMU.R
+++ R/iso_BMU.R
@@ -21,14 +21,14 @@ BMU <- function(level){
     x1 <- github.cssegisanddata.covid19(state = "Bermuda", level = 2)
     
     #' - \href{`r repo("ourworldindata.org")`}{Our World in Data}:
-    #' tests,
     #' total vaccine doses administered,
     #' people with at least one vaccine dose,
     #' people fully vaccinated,
     #' hospitalizations,
     #' intensive care.
     #'
-    x2 <- ourworldindata.org(id = "BMU")
+    x2 <- ourworldindata.org(id = "BMU") %>%
+      select(-c("tests"))
     
     # merge
     x <- full_join(x1, x2, by = "date")
