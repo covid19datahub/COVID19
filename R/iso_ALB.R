f63764629ae790b29a6d12194d813e8c0c208c93
ViktoriiaTan <viktantsiura@gmail.com>
2025-02-17 21:46:51 +0100

    Fixed the code for Albania

--- R/iso_ALB.R
+++ R/iso_ALB.R
@@ -19,6 +19,14 @@ ALB <- function(level){
     #' recovered.
     #'
     x1 <- github.cssegisanddata.covid19(country = "Albania")
+    x1 <- x1[x1$date <= "2023-03-10",]
+    
+    #' - \href{`r repo("who.int")`}{World Health Organization}:
+    #' confirmed cases,
+    #' deaths.
+    #'
+    x2 <- who.int(level = 1, id = "AL")
+    x2 <- x2[x2$date > "2023-03-10",]
     
     #' - \href{`r repo("ourworldindata.org")`}{Our World in Data}:
     #' tests,
@@ -28,10 +36,11 @@ ALB <- function(level){
     #' hospitalizations,
     #' intensive care.
     #'
-    x2 <- ourworldindata.org(id = "ALB")
+    x3 <- ourworldindata.org(id = "ALB")
     
     # merge
-    x <- full_join(x1, x2, by = "date")
+    x <- bind_rows(x1, x2) %>%
+      full_join(x3, by = "date")
     
   }
   
