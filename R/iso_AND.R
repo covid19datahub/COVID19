9f0e8d12f202c974da28ceaca6f5e487f1a96a5b
ViktoriiaTan <viktantsiura@gmail.com>
2025-02-18 11:37:28 +0100

    Fixed the code for Andorra

--- R/iso_AND.R
+++ R/iso_AND.R
@@ -19,6 +19,14 @@ AND <- function(level){
     #' recovered.
     #'
     x1 <- github.cssegisanddata.covid19(country = "Andorra")
+    x1 <- x1[x1$date <= "2023-03-10",]
+    
+    #' - \href{`r repo("who.int")`}{World Health Organization}:
+    #' confirmed cases,
+    #' deaths.
+    #'
+    x2 <- who.int(level = 1, id = "AD")
+    x2 <- x2[x2$date > "2023-03-10",]
     
     #' - \href{`r repo("ourworldindata.org")`}{Our World in Data}:
     #' tests,
@@ -28,11 +36,11 @@ AND <- function(level){
     #' hospitalizations,
     #' intensive care.
     #'
-    x2 <- ourworldindata.org(id = "AND")
+    x3 <- ourworldindata.org(id = "AND")
     
     # merge
-    x <- full_join(x1, x2, by = "date")
-    
+    x <- bind_rows(x1, x2) %>%
+      full_join(x3, by = "date")
   }
   
   return(x)
