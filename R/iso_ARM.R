97e6c86af03cb8c8b933025e3029fd7dac5e7592
ViktoriiaTan <viktantsiura@gmail.com>
2025-02-20 15:21:21 +0100

    Fixed the code for Armenia

--- R/iso_ARM.R
+++ R/iso_ARM.R
@@ -19,6 +19,14 @@ ARM <- function(level){
     #' recovered.
     #'
     x1 <- github.cssegisanddata.covid19(country = "Armenia")
+    x1 <- x1[x1$date <= "2023-03-10",]
+    
+    #' - \href{`r repo("who.int")`}{World Health Organization}:
+    #' confirmed cases,
+    #' deaths.
+    #'
+    x2 <- who.int(level = 1, id = "AM")
+    x2 <- x2[x2$date > "2023-03-10",]
     
     #' - \href{`r repo("ourworldindata.org")`}{Our World in Data}:
     #' tests,
@@ -28,10 +36,11 @@ ARM <- function(level){
     #' hospitalizations,
     #' intensive care.
     #'
-    x2 <- ourworldindata.org(id = "ARM")
+    x3 <- ourworldindata.org(id = "ARM")
     
     # merge
-    x <- full_join(x1, x2, by = "date")
+    x <- bind_rows(x1, x2) %>%
+      full_join(x3, by = "date")
     
   }
   
