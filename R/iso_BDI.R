ad8ddce553476b3856f881bff93591e0efacfd5f
ViktoriiaTan <viktantsiura@gmail.com>
2025-03-01 15:12:35 +0100

    Fixed the code for Burundi

--- R/iso_BDI.R
+++ R/iso_BDI.R
@@ -14,11 +14,16 @@ BDI <- function(level){
   if(level==1){
     
     #' - \href{`r repo("github.cssegisanddata.covid19")`}{Johns Hopkins Center for Systems Science and Engineering}:
-    #' confirmed cases,
-    #' deaths,
-    #' recovered.
+    #' recovered
     #'
-    x1 <- github.cssegisanddata.covid19(country = "Burundi")
+    x1 <- github.cssegisanddata.covid19(country = "Burundi") %>% 
+      select(-c("confirmed", "deaths"))
+    
+    #' - \href{`r repo("who.int")`}{World Health Organization}:
+    #' confirmed cases,
+    #' deaths.
+    #'
+    x2 <- who.int(level = 1, id = "BI")
     
     #' - \href{`r repo("ourworldindata.org")`}{Our World in Data}:
     #' tests,
@@ -28,10 +33,11 @@ BDI <- function(level){
     #' hospitalizations,
     #' intensive care.
     #'
-    x2 <- ourworldindata.org(id = "BDI")
+    x3 <- ourworldindata.org(id = "BDI")
     
     # merge
-    x <- full_join(x1, x2, by = "date")
+    x <- full_join(x1, x2, by = "date") %>%
+      full_join(x3, by = "date")
     
   }
   
