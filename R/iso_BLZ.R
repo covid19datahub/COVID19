c57c5049fd9d009c7a4bb82ea2474102856e0ffa
ViktoriiaTan <viktantsiura@gmail.com>
2025-03-09 10:47:39 +0100

    Fixed the code for Belize

--- R/iso_BLZ.R
+++ R/iso_BLZ.R
@@ -19,13 +19,14 @@ BLZ <- function(level){
     #' recovered.
     #'
     x1 <- github.cssegisanddata.covid19(country = "Belize")
+    x1 <- x1[x1$date <= "2023-03-10",]
     
     #' - \href{`r repo("who.int")`}{World Health Organization}:
-    #' confirmed cases,
-    #' deaths.
+    #' confirmed cases.
     #'
     x2 <- who.int(level = 1, id = "BZ") %>% 
-      filter(date > "2023-03-09")
+      select(-deaths)
+    x2 <- x2[x2$date > "2023-03-10",]
     
     #' - \href{`r repo("ourworldindata.org")`}{Our World in Data}:
     #' tests,
