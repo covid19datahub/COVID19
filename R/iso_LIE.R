8b5d7bacc2c16855a08d5bc12c3f12d7444b9800
ViktoriiaTan <viktantsiura@gmail.com>
2025-03-11 21:21:31 +0100

    Fixed the code for Liechtenstein

--- R/iso_LIE.R
+++ R/iso_LIE.R
@@ -12,28 +12,44 @@ LIE <- function(level){
   #' `r docstring("LIE", 1)`
   #' 
   if(level==1){
-    
+    #' Due to changes in the original file,  
+    #' - \href{`r repo("covid19datahub.io")`}{COVID-19 Data Hub}  
+    #' now provides historical data, which was previously sourced from:  
+    #'  
     #' - \href{`r repo("admin.ch")`}{Federal Office of Public Health}:
     #' confirmed cases,
     #' deaths,
     #' tests,
     #' total vaccine doses administered,
     #' people with at least one vaccine dose,
-    #' people fully vaccinated,
-    #' intensive care.
+    #' people fully vaccinated.
     #'
-    x1 <- admin.ch(state = "FL", level = 1)
+    x1 <- admin.ch(state = "FL", level = 1) %>% 
+      select(-hosp)
+    
+    #' people fully vaccinated.
+    #' 
+    x2 <- covid19datahub.io(iso = "LIE", level) %>% 
+      select(date, people_fully_vaccinated)
+    
+    #' - \href{`r repo("who.int")`}{World Health Organization}:
+    #' confirmed cases,
+    #' deaths.
+    #'
+    x3 <- who.int(level = 1, id = "LI") %>% 
+      filter(date > "2023-11-28")
     
     #' - \href{`r repo("github.openzh.covid19")`}{Specialist Unit for Open Government Data Canton of Zurich}:
     #' recovered,
-    #' hospitalizations,
-    #' patients requiring ventilation.
+    #' hospitalizations.
     #'
-    x2 <- github.openzh.covid19(state = "FL", level = 1) %>%
+    x4 <- github.openzh.covid19(state = "FL", level = 1) %>%
       select(-c("confirmed", "deaths", "tests", "icu"))
     
     # merge
-    x <- full_join(x1, x2, by = "date")
+    x <- bind_rows(x1, x3) %>% 
+      full_join(x2, by = "date") %>% 
+      full_join(x4, by = "date")
     
   }
   
