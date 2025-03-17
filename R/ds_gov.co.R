43e90600c4c27296d8f5d9accb6cd3665a1d0073
ViktoriiaTan <viktantsiura@gmail.com>
2025-03-17 23:32:51 +0100

    Fixed the code for Colombia

--- R/ds_gov.co.R
+++ R/ds_gov.co.R
@@ -8,7 +8,6 @@
 #' - confirmed cases
 #' - deaths
 #' - recovered
-#' - tests
 #'
 #' @section Level 2:
 #' - confirmed cases
@@ -19,9 +18,6 @@
 #' @section Level 3:
 #' - confirmed cases
 #' - deaths
-#' - recovered
-#' - people with at least one vaccine dose
-#' - people fully vaccinated
 #'
 #' @source https://www.datos.gov.co/browse?category=Salud+y+Protecci%C3%B3n+Social&q=covid&sortBy=relevance
 #'
@@ -29,7 +25,7 @@
 #'
 gov.co <- function(level){
   if(!level %in% 1:3) return(NULL)
-  
+  options(timeout = 280)  
   # confirmed, recovered, deaths
   # the number of rows in this file matches the number of confirmed cases reported by other sources (e.g. JHU)
   # this file can be aggregated to compute confirmed, recovered, and deaths at all levels
@@ -51,7 +47,7 @@ gov.co <- function(level){
     # pivot
     pivot_longer(cols = starts_with("date"), names_to = "type", values_to = "date", values_drop_na = TRUE, names_prefix = "date_") %>%
     # convert date
-    mutate(date = as.Date(date, format = "%d/%m/%Y")) %>%
+    mutate(date = as.Date(date)) %>%
     # keep only valid dates
     filter(!is.na(date)) %>%
     # group by city, date, and type of metrics
@@ -59,72 +55,21 @@ gov.co <- function(level){
     # compute the counts for each metric
     summarise(n = n())
   
-  if(level==1 | level==2){
-    
-    # pcr tests
-    # see https://www.datos.gov.co/Salud-y-Protecci-n-Social/Pruebas-PCR-procesadas-de-COVID-19-en-Colombia-Dep/8835-5baf
-    url <- 'https://www.datos.gov.co/resource/8835-5baf.csv?$limit=9999999999'
-    x.pcr <- read.csv(url, encoding="UTF-8")
-    # format and sanitize
-    x.pcr <- x.pcr %>%
-      rename(date = fecha) %>%
-      mutate(date = as.Date(date, format = "%Y-%m-%d")) %>%
-      filter(!is.na(date)) %>%
-      select(-c("cartagena", "barranquilla", "santa_marta",
-                "indeterminadas", "procedencia_desconocida",
-                "positivas_acumuladas", "negativas_acumuladas", 
-                "positividad_acumulada")) %>%
-      pivot_longer(cols = -1, names_to = 'state', values_to = 'n', values_drop_na = TRUE)
-    
-    # antigen tests
-    # see https://www.datos.gov.co/Salud-y-Protecci-n-Social/Ant-geno-procesadas-de-COVID-19-en-Colombia-Depart/ci85-cyhe
-    url <- 'https://www.datos.gov.co/resource/ci85-cyhe.csv?$limit=9999999999'
-    x.ant <- read.csv(url, encoding="UTF-8")
-    # format and sanitize (make names compatible with the PCR dataset)
-    x.ant <- x.ant %>%
-      rename(
-        date = mes,
-        narino = nari_o, 
-        norte_de_santander = norte_santander,
-        bogota = bogota_dc) %>%
-      mutate(date = as.Date(date, format = "%Y-%m-%d")) %>%
-      filter(!is.na(date)) %>%
-      pivot_longer(cols = -1, names_to = 'state', values_to = 'n', values_drop_na = TRUE)
-    
     if(level==1){
       
       # confirmed, recovered, deaths
-      x.cases <- x.cases %>%
+      x <- x.cases %>%
         group_by(date, type) %>%
         summarise(n = sum(n)) %>%
         group_by(type) %>%
         arrange(date) %>%
         mutate(n = cumsum(n)) %>%
         pivot_wider(id_cols = "date", names_from = "type", values_from = "n")
-      
-      # pcr tests
-      x.pcr <- x.pcr %>%
-        filter(state=="acumuladas") %>%
-        rename(pcr = n)
-      
-      # antigen tests
-      x.ant <- x.ant %>%
-        group_by(date) %>%
-        summarise(antigen = sum(n))
-      
-      # merge 
-      by <- "date"
-      x <- x.cases %>%
-        full_join(x.pcr, by = by) %>%
-        full_join(x.ant, by = by) %>%
-        mutate(tests = antigen + pcr)
-      
     }
-    
+  
     if(level==2){
-      
       # confirmed, recovered, deaths
-      x.cases <- x.cases %>%
+      x <- x.cases %>%
         filter(!state_code %in% c(13001, 8001, 47001)) %>%
         group_by(date, state_code, type) %>%
         summarise(n = sum(n)) %>%
@@ -132,66 +77,24 @@ gov.co <- function(level){
         arrange(date) %>%
         mutate(n = cumsum(n)) %>%
         pivot_wider(id_cols = c("date", "state_code"), names_from = "type", values_from = "n")
-
-      # pcr tests
-      x.pcr <- x.pcr %>%
-        filter(state!="acumuladas") %>%
-        rename(pcr = n)
-      
-      # antigen tests
-      x.ant <- x.ant %>%
-        rename(antigen = n)
       
       # map code to state as used in the PCR and antigen datasets      
-      db <- extdata("db/COL.csv")
+      db <- system.file("extdata", "db/COL.csv", package = "COVID19")
+      db <- read.csv(db, encoding = "UTF-8")
       idx <- which(db$administrative_area_level==2)
       map <- db$id_gov.co[idx]
       names(map) <- as.integer(db$key_local[idx])
-      x.cases$state <- map_values(x.cases$state_code, map = map)
-      
-      # merge 
-      by <- c("date", "state")
-      x <- x.cases %>%
-        full_join(x.pcr, by = by) %>%
-        full_join(x.ant, by = by) %>%
-        mutate(tests = antigen + pcr)
-      
-    }
+      x$state <- map_values(x$state_code, map = map)
     
   }
   
   if(level==3){
-    
-    # this file does not sum up to the vaccines for level 1 (from e.g. Our World in Data)
-    # use only for level 3 as it is: do not aggregate to upper levels
-    # see https://www.datos.gov.co/Salud-y-Protecci-n-Social/Coberturas-de-Vacunaci-n-contra-COVID-19/8cgj-t5ds
-    url <- 'https://www.datos.gov.co/resource/8cgj-t5ds.csv?$limit=9999999999'
-    x.vacc <- read.csv(url, encoding="UTF-8")
-    # format
-    x.vacc <- map_data(x.vacc, c(
-      "fecha_de_corte"     = "date",
-      "municipio_divipola" = "city_code",
-      "grupo_edad"         = "age",
-      "n_mero_acumulado_de_1_dosis" = "people_vaccinated",
-      "n_mero_acumulado_de_esquema" = "people_fully_vaccinated"
-    ))
-    # sanitize
-    x.vacc <- x.vacc %>%
-      mutate(date = as.Date(date, format = "%Y-%m-%d")) %>%
-      filter(age=="Todas" & city_code!="No definido" & !is.na(date)) %>%
-      mutate(city_code = as.integer(city_code))
-      
     # confirmed, recovered, deaths
-    x.cases <- x.cases %>%
+    x <- x.cases %>%
       group_by(type, city_code) %>%
       arrange(date) %>%
       mutate(n = cumsum(n)) %>%
       pivot_wider(id_cols = c("date", "city_code"), names_from = "type", values_from = "n")
-    
-    # merge
-    by <- c("date", "city_code")
-    x <- x.cases %>%
-      full_join(x.vacc, by = by)
     
   }
   
--- R/iso_COL.R
+++ R/iso_COL.R
@@ -12,24 +12,23 @@ COL <- function(level){
   #' `r docstring("COL", 1)`
   #' 
   if(level==1){
-    
+
     #' - \href{`r repo("gov.co")`}{Instituto Nacional de Salud}:
     #' confirmed cases,
     #' deaths,
-    #' recovered,
-    #' tests.
+    #' recovered.
     #'
     x1 <- gov.co(level = level)
     
     #' - \href{`r repo("ourworldindata.org")`}{Our World in Data}:
+    #' tests,
     #' total vaccine doses administered,
     #' people with at least one vaccine dose,
     #' people fully vaccinated,
     #' hospitalizations,
     #' intensive care.
     #'
-    x2 <- ourworldindata.org(id = "COL") %>%
-      select(-c("tests"))
+    x2 <- ourworldindata.org(id = "COL")
     
     # merge
     x <- full_join(x1, x2, by = "date")
@@ -43,16 +42,25 @@ COL <- function(level){
   #' `r docstring("COL", 2)`
   #' 
   if(level==2){
-    
+    #' Due to changes in the original file,  
+    #' - \href{`r repo("covid19datahub.io")`}{COVID-19 Data Hub}  
+    #' now provides historical data, which was previously sourced from:  
+    #' 
     #' - \href{`r repo("gov.co")`}{Instituto Nacional de Salud}:
     #' confirmed cases,
     #' deaths,
     #' recovered,
     #' tests.
     #'
-    x <- gov.co(level = level)
-    x$id <- id(x$state, iso = "COL", ds = "gov.co", level = level)
+    x1 <- gov.co(level = level)
+    x1$id <- id(x1$state, iso = "COL", ds = "gov.co", level = level)
     
+    x2 <- covid19datahub.io(iso = "COL", level) %>% 
+      select(id, date, tests)
+    
+    # merge
+    x <- full_join(x1, x2, by = c("date", "id"))
+
   }
   
   #' @concept Level 3
@@ -62,7 +70,10 @@ COL <- function(level){
   #' `r docstring("COL", 3)`
   #' 
   if(level==3){  
-    
+    #' Due to changes in the original file,  
+    #' - \href{`r repo("covid19datahub.io")`}{COVID-19 Data Hub}  
+    #' now provides historical data, which was previously sourced from:  
+    #' 
     #' - \href{`r repo("gov.co")`}{Instituto Nacional de Salud}:
     #' confirmed cases,
     #' deaths,
@@ -70,9 +81,14 @@ COL <- function(level){
     #' people with at least one vaccine dose,
     #' people fully vaccinated.
     #'
-    x <- gov.co(level = level)
-    x$id <- id(x$city_code, iso = "COL", ds = "gov.co", level = level)
+    x1 <- gov.co(level = level)
+    x1$id <- id(x1$city_code, iso = "COL", ds = "gov.co", level = level)
     
+    x2 <- covid19datahub.io(iso = "COL", level) %>% 
+      select(id, date, people_vaccinated, people_fully_vaccinated)
+    
+    # merge
+    x <- full_join(x1, x2, by = c("date", "id"))
   }
   
   return(x)
