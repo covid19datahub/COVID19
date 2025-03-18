2325329fd358f0a1c6b0196a3a661d62a82f75f1
ViktoriiaTan <viktantsiura@gmail.com>
2025-03-18 12:21:00 +0100

    Fixed the code for Costa Rica

--- R/iso_CRI.R
+++ R/iso_CRI.R
@@ -19,6 +19,13 @@ CRI <- function(level){
     #' recovered.
     #'
     x1 <- github.cssegisanddata.covid19(country = "Costa Rica")
+    x1 <- x1[x1$date <= "2023-03-10",]
+    
+    #' - \href{`r repo("who.int")`}{World Health Organization}:
+    #' confirmed cases.
+    #' deaths.
+    x2 <- who.int(level = 1, id = "CR")
+    x2 <- x2[x2$date > "2023-03-10",]
     
     #' - \href{`r repo("ourworldindata.org")`}{Our World in Data}:
     #' tests,
@@ -28,10 +35,11 @@ CRI <- function(level){
     #' hospitalizations,
     #' intensive care.
     #'
-    x2 <- ourworldindata.org(id = "CRI")
+    x3 <- ourworldindata.org(id = "CRI")
     
     # merge
-    x <- full_join(x1, x2, by = "date")
+    x <- bind_rows(x1, x2) %>%
+      full_join(x3, by = "date")
     
   }
   
