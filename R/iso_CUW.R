7ae19b72ac212267643eb399ba986ade249812be
ViktoriiaTan <viktantsiura@gmail.com>
2025-03-18 12:53:49 +0100

    Fixed the code for Curacao

--- R/iso_CUW.R
+++ R/iso_CUW.R
@@ -19,6 +19,13 @@ CUW <- function(level){
     #' recovered.
     #'
     x1 <- github.cssegisanddata.covid19(state = "Curacao", level = 2)
+    x1 <- x1[x1$date <= "2023-03-10",]
+    
+    #' - \href{`r repo("who.int")`}{World Health Organization}:
+    #' confirmed cases.
+    #' deaths.
+    x2 <- who.int(level = 1, id = "CW")
+    x2 <- x2[x2$date > "2023-03-10",]
     
     #' - \href{`r repo("ourworldindata.org")`}{Our World in Data}:
     #' total vaccine doses administered,
@@ -27,11 +34,12 @@ CUW <- function(level){
     #' hospitalizations,
     #' intensive care.
     #'
-    x2 <- ourworldindata.org(id = "CUW") %>%
+    x3 <- ourworldindata.org(id = "CUW") %>%
       select(-c("tests"))
     
     # merge
-    x <- full_join(x1, x2, by = "date")
+    x <- bind_rows(x1, x2) %>%
+      full_join(x3, by = "date")
     
   }
   
