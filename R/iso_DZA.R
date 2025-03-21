7bd0af7c7969f3af93c7cebe3c728f18d9ec0d39
ViktoriiaTan <viktantsiura@gmail.com>
2025-03-21 17:26:34 +0100

    Fixed the code for Algeria

--- R/iso_DZA.R
+++ R/iso_DZA.R
@@ -19,6 +19,12 @@ DZA <- function(level){
     #' recovered.
     #'
     x1 <- github.cssegisanddata.covid19(country = "Algeria")
+    x1 <- x1[x1$date <= "2023-03-10",]
+    
+    #' - \href{`r repo("who.int")`}{World Health Organization}:
+    #' confirmed cases.
+    x2 <- who.int(level = 1, id = "DZ")
+    x2 <- x2[x2$date > "2023-03-10",]
     
     #' - \href{`r repo("ourworldindata.org")`}{Our World in Data}:
     #' total vaccine doses administered,
@@ -27,11 +33,12 @@ DZA <- function(level){
     #' hospitalizations,
     #' intensive care.
     #'
-    x2 <- ourworldindata.org(id = "DZA") %>%
+    x3 <- ourworldindata.org(id = "DZA") %>%
       select(-c("tests"))
     
     # merge
-    x <- full_join(x1, x2, by = "date")
+    x <- bind_rows(x1, x2) %>%
+      full_join(x3, by = "date")
     
   }
   
