2637c12a3a9a3819815b56391dc8a453573ae8fd
ViktoriiaTan <viktantsiura@gmail.com>
2025-04-16 21:54:45 +0200

    Fixed the code for Pakistan

--- R/iso_PAK.R
+++ R/iso_PAK.R
@@ -19,6 +19,14 @@ PAK <- function(level){
     #' recovered.
     #'
     x1 <- github.cssegisanddata.covid19(country = "Pakistan")
+    x1 <- x1[x1$date <= "2023-03-10",]
+    
+    #' - \href{`r repo("who.int")`}{World Health Organization}:
+    #' confirmed cases,
+    #' deaths.
+    #' 
+    x2 <- who.int(level = 1, id = "PK")
+    x2 <- x2[x2$date > "2023-03-10",]
     
     #' - \href{`r repo("ourworldindata.org")`}{Our World in Data}:
     #' tests,
@@ -28,10 +36,11 @@ PAK <- function(level){
     #' hospitalizations,
     #' intensive care.
     #'
-    x2 <- ourworldindata.org(id = "PAK")
+    x3 <- ourworldindata.org(id = "PAK")
     
     # merge
-    x <- full_join(x1, x2, by = "date")
+    x <-bind_rows(x1, x2) %>% 
+      full_join(x3, by = "date")
     
   }
   
