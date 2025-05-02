e39f86ea551d038ac916a78c869a7ad62ac3485e
ViktoriiaTan <viktantsiura@gmail.com>
2025-05-02 18:30:19 +0200

    Fixed code for Guernsey

--- R/iso_GGY.R
+++ R/iso_GGY.R
@@ -12,26 +12,23 @@ GGY <- function(level){
   #' `r docstring("GGY", 1)`
   #' 
   if(level==1){
-    
-    #' - \href{`r repo("github.cssegisanddata.covid19")`}{Johns Hopkins Center for Systems Science and Engineering}:
+
+    #' - \href{`r repo("who.int")`}{World Health Organization}:
     #' confirmed cases,
-    #' deaths,
-    #' recovered.
-    #'
-    x1 <- github.cssegisanddata.covid19(country = "Guernsey")
+    #' deaths.
+    #' 
+    x1 <- who.int(level, id = "GG")
     
     #' - \href{`r repo("ourworldindata.org")`}{Our World in Data}:
-    #' tests,
     #' total vaccine doses administered,
     #' people with at least one vaccine dose,
-    #' people fully vaccinated,
-    #' hospitalizations,
-    #' intensive care.
+    #' people fully vaccinated.
     #'
     x2 <- ourworldindata.org(id = "GGY")
     
     # merge
-    x <- full_join(x1, x2, by = "date")
+    x <-  full_join(x1, x2, by = "date")
+     
     
   }
   
