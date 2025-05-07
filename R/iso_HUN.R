14c6ee800af34084d5466cb60699e1f9da55609b
ViktoriiaTan <viktantsiura@gmail.com>
2025-05-07 21:13:13 +0200

    Fixed code for Hungary

--- R/iso_HUN.R
+++ R/iso_HUN.R
@@ -19,19 +19,33 @@ HUN <- function(level){
     #' recovered.
     #'
     x1 <- github.cssegisanddata.covid19(country = "Hungary")
+    x1 <- x1[x1$date <= "2023-03-10",]
+    
+    #' - \href{`r repo("who.int")`}{World Health Organization}:
+    #' confirmed cases,
+    #' deaths.
+    #' 
+    x2 <- who.int(level, id = "HU")
+    x2 <- x2[x2$date > "2023-03-10",]
     
     #' - \href{`r repo("ourworldindata.org")`}{Our World in Data}:
     #' tests,
     #' total vaccine doses administered,
     #' people with at least one vaccine dose,
     #' people fully vaccinated,
-    #' hospitalizations,
-    #' intensive care.
+    #' hospitalizations.
     #'
-    x2 <- ourworldindata.org(id = "HUN")
+    x3 <- ourworldindata.org(id = "HUN") %>% 
+      select(-hosp)
+    
+    # use vintage data because hosp data from ourworldindata.org is no longer available
+    x4 <- covid19datahub.io(iso = "HUN", level) %>% 
+      select(date, hosp)
     
     # merge
-    x <- full_join(x1, x2, by = "date")
+    x <- bind_rows(x1, x2) %>%
+      full_join(x3, by = "date") %>%
+      full_join(x4, by = "date")
     
   }
   
