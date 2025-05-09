5923162914fc29fc2f70dd78532101ed49f5e187
ViktoriiaTan <viktantsiura@gmail.com>
2025-05-09 10:50:52 +0200

    Fixed code for Jersey

--- R/iso_JEY.R
+++ R/iso_JEY.R
@@ -15,18 +15,14 @@ JEY <- function(level){
     
     #' - \href{`r repo("github.cssegisanddata.covid19")`}{Johns Hopkins Center for Systems Science and Engineering}:
     #' confirmed cases,
-    #' deaths,
-    #' recovered.
+    #' deaths.
     #'
-    x1 <- github.cssegisanddata.covid19(country = "Jersey")
+    x1 <- github.cssegisanddata.covid19(country = "United Kingdom", state = "Jersey", level = 2)
     
     #' - \href{`r repo("ourworldindata.org")`}{Our World in Data}:
-    #' tests,
     #' total vaccine doses administered,
     #' people with at least one vaccine dose,
-    #' people fully vaccinated,
-    #' hospitalizations,
-    #' intensive care.
+    #' people fully vaccinated.
     #'
     x2 <- ourworldindata.org(id = "JEY")
     
