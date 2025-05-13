507a690723720e0732b6cbbc0edc56ef11bba9c6
ViktoriiaTan <viktantsiura@gmail.com>
2025-05-13 11:10:56 +0200

    Fixed code for Madagascar

--- R/iso_MDG.R
+++ R/iso_MDG.R
@@ -19,20 +19,25 @@ MDG <- function(level){
     #' recovered.
     #'
     x1 <- github.cssegisanddata.covid19(country = "Madagascar")
+    x1 <- x1[x1$date <= "2023-03-10",]
+    
+    #' - \href{`r repo("who.int")`}{World Health Organization}:
+    #' confirmed cases,
+    #' deaths.
+    x2 <- who.int(level, id = "MG") 
+    x2 <- x2[x2$date > "2023-03-10",]
     
     #' - \href{`r repo("ourworldindata.org")`}{Our World in Data}:
     #' tests,
     #' total vaccine doses administered,
     #' people with at least one vaccine dose,
-    #' people fully vaccinated,
-    #' hospitalizations,
-    #' intensive care.
+    #' people fully vaccinated.
     #'
-    x2 <- ourworldindata.org(id = "MDG")
+    x3 <- ourworldindata.org(id = "MDG")
     
     # merge
-    x <- full_join(x1, x2, by = "date")
-    
+    x <- bind_rows(x1, x2) %>%
+      full_join(x3, by = "date")
   }
   
   return(x)
