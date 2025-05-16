313f39600017fdfeb9d7be5084d702a168c5b341
ViktoriiaTan <viktantsiura@gmail.com>
2025-05-16 15:28:32 +0200

    Fixed code for Romania

--- R/iso_ROU.R
+++ R/iso_ROU.R
@@ -19,6 +19,14 @@ ROU <- function(level){
     #' recovered.
     #'
     x1 <- github.cssegisanddata.covid19(country = "Romania")
+    x1 <- x1[x1$date <= "2023-03-10",]
+    
+    #' - \href{`r repo("who.int")`}{World Health Organization}:
+    #' confirmed cases,
+    #' deaths.
+    #' 
+    x2 <- who.int(level, id = "RO") 
+    x2 <- x2[x2$date > "2023-03-10",]
     
     #' - \href{`r repo("ourworldindata.org")`}{Our World in Data}:
     #' tests,
@@ -28,10 +36,17 @@ ROU <- function(level){
     #' hospitalizations,
     #' intensive care.
     #'
-    x2 <- ourworldindata.org(id = "ROU")
+    x3 <- ourworldindata.org(id = "ROU")
+    
+    # use vintage data because part of daily icu data from ourworldindata.org is no longer available
+    x4 <- covid19datahub.io(iso = "ROU", level) %>% 
+      select(date, icu)
     
     # merge
-    x <- full_join(x1, x2, by = "date")
+    x <- bind_rows(x1, x2) %>%
+      full_join(x3, by = "date") %>% 
+      full_join(x4, by = "date") %>% 
+      mutate(icu = coalesce(icu.y, icu.x))
     
   }
   
