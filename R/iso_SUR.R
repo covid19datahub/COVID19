1d0a879ba73bb07fd9c395a70f6cf8889c0db767
ViktoriiaTan <viktantsiura@gmail.com>
2025-05-21 11:48:13 +0200

    Fixed code for Suriname

--- R/iso_SUR.R
+++ R/iso_SUR.R
@@ -19,19 +19,26 @@ SUR <- function(level){
     #' recovered.
     #'
     x1 <- github.cssegisanddata.covid19(country = "Suriname")
+    x1 <- x1[x1$date <= "2023-03-10",]
+    
+    #' - \href{`r repo("who.int")`}{World Health Organization}:
+    #' confirmed cases,
+    #' deaths.
+    #' 
+    x2 <- who.int(level, id = "SR")
+    x2 <- x2[x2$date > "2023-03-10",]
     
     #' - \href{`r repo("ourworldindata.org")`}{Our World in Data}:
-    #' tests,
     #' total vaccine doses administered,
     #' people with at least one vaccine dose,
     #' people fully vaccinated,
-    #' hospitalizations,
-    #' intensive care.
     #'
-    x2 <- ourworldindata.org(id = "SUR")
+    x3 <- ourworldindata.org(id = "SUR") %>% 
+      select(-tests)
     
     # merge
-    x <- full_join(x1, x2, by = "date")
+    x <- bind_rows(x1, x2) %>%
+      full_join(x3, by = "date")
     
   }
   
