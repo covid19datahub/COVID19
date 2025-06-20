1a7dd5ecad5734cc560f25abade660789cb4fa69
Emanuele Guidotti <emanuele.guidotti@unine.ch>
2025-06-20 10:08:37 +0200

    Update iso_AND.R

--- R/iso_AND.R
+++ R/iso_AND.R
@@ -22,10 +22,10 @@ AND <- function(level){
     x1 <- x1[x1$date <= "2023-03-10",]
     
     #' - \href{`r repo("who.int")`}{World Health Organization}:
-    #' confirmed cases.
+    #' confirmed cases,
+    #' deaths.
     #'
-    x2 <- who.int(level, id = "AD") %>% 
-      select(-deaths)
+    x2 <- who.int(level, id = "AD")
     x2 <- x2[x2$date > "2023-03-10",]
     
     #' - \href{`r repo("ourworldindata.org")`}{Our World in Data}:
